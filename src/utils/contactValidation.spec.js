dbe6bf8d3953944874ab4fad5bc7d9eefcf15709
Marcos Duque <mduqueoviedo@gmail.com>
2019-04-25 00:58:10 +0200

    contactValidation tests

--- src/utils/contactValidation.spec.js
+++ src/utils/contactValidation.spec.js
@@ -1,17 +1,38 @@
-describe("validateContact", () => {
-    it("returns empty array when no errors", () => {
+import { validateContact } from './contactValidation';
 
+describe("validateContact", () => {
+    const validContact = {
+        firstName: 'Max', 
+        lastName: 'Mustermann', 
+        email: 'mail@example.com', 
+        country: 'es'
+    };
+
+    it("returns empty array when no errors", () => {
+        expect(validateContact(validContact)).toEqual([]);
     });
 
     it ("validates empty fields", () => {
-
+        const invalidFirstNameContact = {...validContact, firstName: ''};
+        const invalidLastNameContact = {...validContact, lastName: ''};
+        const invalidEmailContact = {...validContact, email: ''};
+        const invalidCountryContact = {...validContact, country: ''};
+        
+        expect(validateContact(invalidFirstNameContact)).toEqual([{firstName: 'required'}]);
+        expect(validateContact(invalidLastNameContact)).toEqual([{lastName: 'required'}]);
+        expect(validateContact(invalidEmailContact)).toEqual([{email: 'required'}, {email: 'invalid'}]);
+        expect(validateContact(invalidCountryContact)).toEqual([{country: 'required'}]);
     });
 
-    it ("validates invalid names", () => {
+    it("validates email format", () => {
+        const invalidEmailContact1 = {...validContact, email: 'wrongMail'};
+        const invalidEmailContact2 = {...validContact, email: 'wrongMail@mail'};
+        const invalidEmailContact3 = {...validContact, email: 'wrongMail@mail.'};
+        const invalidEmailContact4 = {...validContact, email: '@mail.com'};
 
-    });
-
-    it("validates email", () => {
-
+        expect(validateContact(invalidEmailContact1)).toEqual([{email: 'invalid'}]);
+        expect(validateContact(invalidEmailContact2)).toEqual([{email: 'invalid'}]);
+        expect(validateContact(invalidEmailContact3)).toEqual([{email: 'invalid'}]);
+        expect(validateContact(invalidEmailContact4)).toEqual([{email: 'invalid'}]);
     });
 })
