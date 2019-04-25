8c990c33c00e3ae6cad009bffc83474173cd4b11
Marcos Duque <mduqueoviedo@gmail.com>
2019-04-25 03:14:29 +0200

    Fix imports

--- src/init.js
+++ src/init.js
@@ -1,7 +1,23 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
+import {
+  faUserPlus,
+  faEdit,
+  faTrashAlt,
+  faGlobeEurope,
+  faTimes,
+  faExclamationTriangle,
+} from '@fortawesome/free-solid-svg-icons';
+import { faUser, faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
 export const initFontAwesomeIcons = () => {
-  library.add(fas, far);
+  library.add(
+    faUserPlus,
+    faEdit,
+    faTrashAlt,
+    faGlobeEurope,
+    faTimes,
+    faUser,
+    faEnvelope,
+    faExclamationTriangle
+  );
 };
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,7 +3,3 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
-
-const Dexie = require('dexie');
-Dexie.dependencies.indexedDB = require('fake-indexeddb');
-Dexie.dependencies.IDBKeyRange = require('fake-indexeddb/lib/FDBKeyRange');
